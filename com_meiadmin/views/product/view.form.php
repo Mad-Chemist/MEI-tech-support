fe44902ddcfef65e11b4a59ff19fd3b11511a78e
Vico Bertogli <vico@b3wd.com>
2014-01-09 11:24:06 -0500

    Added header
    <h1>Products</h1>

--- com_meiadmin/views/product/view.form.php
+++ com_meiadmin/views/product/view.form.php
@@ -7,7 +7,9 @@ class MeiAdminViewProduct extends BBDFOFViewForm {
     public function display($tpl = null){
         JHtmlBootstrap::framework();
         $this->_loadFiles();
+        echo "<h1>Products</h1>";
         return parent::display($tpl);
+
     }
 
     protected function _loadFiles(){
