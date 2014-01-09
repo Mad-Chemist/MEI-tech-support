cad0d5f7624f375dd517721bada3b640c2e35c90
Vico Bertogli <vico@b3wd.com>
2014-01-09 11:23:39 -0500

    Added header
    <h1>Product Families</h1>

--- com_meiadmin/controllers/family.php
+++ com_meiadmin/controllers/family.php
@@ -7,6 +7,7 @@ class MeiadminControllerFamily extends BBDFOFController {
 	public function display($cachable = false, $urlparams = false) {
 		$view = $this->getThisView(array('tbl' => '#__meiadmin_categories',
 										 'table' => 'categories'));
+		echo "<h1>Product Families</h1><hr />";
 		parent::display();
 	}
 
