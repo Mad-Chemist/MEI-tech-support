e62f09d8b7613085426110d0593703a29c05c310
Vico Bertogli <vico@b3wd.com>
2014-01-15 14:16:28 -0500

    User icon upload
    can now upload a user icon during account creation process

--- meisupport/includes/get-user-icon.php
+++ meisupport/includes/get-user-icon.php
@@ -2,7 +2,7 @@
 	$getuserImage 		=	mysql_query('SELECT  `image` FROM  `44aae_meiadmin_customers` WHERE  `fk_user_id` = '.$user->id); //grabs all products
 	$getuserImage 		= 	mysql_fetch_assoc($getuserImage);  
 	$getuserImage 		= 	$getuserImage['image'];
-	if (strlen($getuserImage) > 0) echo '<div class="userImg"><img src="/images/user-images/'.$getuserImage.'"></div>';
+	if (strlen($getuserImage) > 0) echo '<div class="userImg"><img src="'.$getuserImage.'"></div>';
 	else echo '<div class="userImg"><img src="/images/user-images/mei-default.jpg"></div>';
 ?>
 
