1beed4ed0690f4cd717bcd17f3bfb2f68340046c
Vico Bertogli <vico@b3wd.com>
2014-01-23 17:26:19 -0500

    added link when no prof image

--- meisupport/includes/get-user-icon.php
+++ meisupport/includes/get-user-icon.php
@@ -3,6 +3,6 @@
 	$getuserImage 		= 	mysql_fetch_assoc($getuserImage);  
 	$getuserImage 		= 	$getuserImage['image'];
 	if (strlen($getuserImage) > 0) echo '<div class="userImg"><a href="/index.php?option=com_mei&view=customer&layout=form&Itemid=212" title="Change your Icon"><img src="'.$getuserImage.'"></a></div>';
-	else echo '<div class="userImg"><img src="/images/user-images/mei-default.jpg"></div>';
+	else echo '<div class="userImg"><a href="/index.php?option=com_mei&view=customer&layout=form&Itemid=212" title="Change your Icon"><img src="/images/user-images/mei-default.jpg"></a></div>';
 ?>
 
