24f61c388f8648165ca9cb230285ae78b2aa8402
Vico Bertogli <vico@b3wd.com>
2014-02-20 15:06:14 -0500

    increased var

--- com_mei/views/product/tmpl/item.php
+++ com_mei/views/product/tmpl/item.php
@@ -4,9 +4,9 @@ defined('_JEXEC') or die();
 $userV3 =& JFactory::getUser();
 $userId = $userV3->get( 'id' );
 if ($userId > 100) { 
-    $userAccess     =   mysql_query('SELECT  `fk_region_id`, `fk_channel_id` FROM  `44aae_meiadmin_customers` WHERE  `fk_user_id` = '.$userId.' LIMIT 0,1');
+    $userAccess      =   mysql_query('SELECT  `fk_region_id`, `fk_channel_id` FROM  `44aae_meiadmin_customers` WHERE  `fk_user_id` = '.$userId.' LIMIT 0,1');
     if ($userAccess) {
-        while ($row     =   mysql_fetch_array($userAccess) ){ 
+        while ($row  =   mysql_fetch_array($userAccess) ){ 
             $GLOBALS['regV'] = $row[0];
             $GLOBALS['chanV'] = $row[1];
         }
@@ -49,7 +49,7 @@ $GLOBALS['PERMV'] = true;
     $productId = $jinput->get('id', 0, 'alnum');
 
     $inputvars = array(
-        'limit'         => 10,
+        'limit'         => 10000,
         'limitstart'    => 0,
         'fk_product_id' => $productId,
         /*by adding this enabled check, it will prevent showing files that have been disabled.*/
